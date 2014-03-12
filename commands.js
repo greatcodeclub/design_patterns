057fb6da41fc0787c43a9a943b63b8f143d0e097
macournoyer <macournoyer@gmail.com>
2014-03-12 15:08:20 -0400

    Move CommandFactory to a separate file.

--- commands.js
+++ commands.js
@@ -1,29 +1,4 @@
-//// The command factory. Creates the commands.
-
-function CommandFactory($pony) {
-  this.$pony = $pony
-}
-
-CommandFactory.prototype.createCommandFromKeyCode = function(keyCode) {
-  var direction = keyCodeToName[keyCode] // Convert key code to direction name
-
-  if (direction) {
-    // Following lines are the dynamic equivalent to:
-    //    window['MoveUpCommand']
-    // and
-    //    window.MoveUpCommand
-    // and
-    //    MoveUpCommand
-    var capitalizedDirection = direction[0].toUpperCase() + direction.substr(1),
-        commandClass = window['Move' + capitalizedDirection + 'Command']
-
-    return new commandClass(this.$pony)
-  }
-}
-
-
 //// The base class for all commands
-
 function Command($pony) {
   this.$pony = $pony
 }
